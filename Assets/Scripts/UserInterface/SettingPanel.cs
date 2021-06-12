382ef8f50918128f2ae2fe08bf0b03d533b52d4b
Tiến Đức <42690879+mmsbba4@users.noreply.github.com>
2021-06-12 15:25:36 +0700

    ok

--- Assets/Scripts/UserInterface/SettingPanel.cs
+++ Assets/Scripts/UserInterface/SettingPanel.cs
@@ -2,14 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 public class SettingPanel : MonoBehaviour
 {
-    public AudioMixerGroup sound, music;
-    
-    void SetSound()
+    public AudioMixer sound;
+    public Slider sound_sli, music_sli;
+    bool is_music = true;
+    bool is_sound = true;
+    public void SetSound()
     {
-        sound.audioMixer.
+        if (is_music)
+        {
+            sound_sli.value = 0;
+            sound.SetFloat("sound_vol", -80.0f);
+            is_music = false;
+        }
+        else
+        {
+            sound_sli.value = 1;
+            sound.SetFloat("sound_vol", 0f);
+            is_music = true;
+        }
+      //  
+    }
+   public void SetMusic()
+    {
+        if (is_sound)
+        {
+            music_sli.value = 0;
+            sound.SetFloat("music_vol", -80.0f);
+            is_sound = false;
+        }
+        else
+        {
+            music_sli.value = 1;
+            sound.SetFloat("music_vol", 0f);
+            is_sound = true;
+        }
     }
 
 }
